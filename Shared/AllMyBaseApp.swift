4ae3e5868549f8c77c3e424b4d83c03754791e1e
Petr Šíma <ptea.sima@gmail.com>
2021-10-29 12:48:27 +0200

    Initial Commit

---
+++ Shared/AllMyBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllMyBaseApp.swift
+//  Shared
+//
+//  Created by Petr Šíma on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AllMyBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
